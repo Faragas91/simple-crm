f5000aa074efa1fdc5cdc8ecdb8c76fdd723ef37
Stefan Redl <bernhardlaus@gmx.at>
2025-05-30 11:34:12 +0200

    started to conect firebase to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { CommonModule, AsyncPipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -16,10 +18,19 @@ import { RouterLink } from '@angular/router';
     MatSidenavModule,
     MatIconModule, 
     RouterLink,
+    AsyncPipe,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  firestore: Firestore = Inject(Firestore);
+  items$: Observable<any[]>;
+
   title = 'simple-crm';
+
+    constructor() {
+    const aCollection = collection(this.firestore, 'items')
+    this.items$ = collectionData(aCollection);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,10 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideNativeDateAdapter()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideNativeDateAdapter(), provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-d2328","appId":"1:848401370279:web:19c4a848178b3cd1ca1f82","storageBucket":"simple-crm-d2328.firebasestorage.app","apiKey":"AIzaSyCTYr6U1W2NBtuf8GxNXlYPicGGl5dXuDk","authDomain":"simple-crm-d2328.firebaseapp.com","messagingSenderId":"848401370279"})), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
 };
