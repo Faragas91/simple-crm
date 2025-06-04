134b6166de0d805d4188bba1093a7c8d9893e285
Stefan Redl <bernhardlaus@gmx.at>
2025-06-04 20:48:55 +0200

    get user specific information from firebase

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { User } from '../../models/user.class';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Firestore, doc, docData} from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
@@ -14,8 +15,29 @@ import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 })
 export class UserDetailComponent {
 
-  user = new User();
+  user:any = {};
 
-  constructor(private firestore: Firestore) {} 
+  constructor(
+    private firestore: Firestore, 
+    private route: ActivatedRoute
+  ) {} 
   
+  ngOnInit(): void {
+    const userId = this.route.snapshot.paramMap.get('id');
+    console.log('User ID from route:', userId);
+    if (userId) {
+      this.getUser(userId); // <-- userId wird korrekt übergeben
+    }
+  }
+
+  getUser(userId?: string): void {
+      const userDocRef = doc(this.firestore, `users/${userId}`);
+      docData(userDocRef, { idField: 'id' })
+      .subscribe((userData) => {
+        this.user = userData;
+        console.log('User data:', this.user);
+      }, error => {
+        console.error('Error fetching user data:', error);
+      });
+    };
 }
